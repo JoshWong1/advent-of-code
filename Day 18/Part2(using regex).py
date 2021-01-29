d59f38f62c48a41023e5d22307e38c27d174f767
JoshWong1 <wongj09@gmail.com>
2021-01-29 15:09:38 -0500

    Create Part2(using regex).py

---
+++ Day 18/Part2(using regex).py
@@ -0,0 +1,23 @@
+import re
+def solve(s):
+    while "+" in s:
+        r = re.search('(\d)*\+(\d)*', s).group()
+        i = r.index("+")
+        s = s.replace(r, str(int(r[0:i]) + int(r[i+1:])))
+    return eval(s)
+
+def solveP(s):
+    while "(" in s or ")" in s:
+        r = re.search('\([^(^)]*\)', s).group()
+        s = s.replace(r, str(solve(r[1:-1])))
+    return(solve(s))
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        l = [line.strip().replace(" ", "") for line in f]
+
+    sol = 0
+    for s in l:
+        x = solveP(s)        
+        sol += x
+    print(sol)
