bc0c7cb478bba969367829add6ced70539b007c6
JoshWong1 <wongj09@gmail.com>
2021-01-29 00:48:01 -0500

    Create Part1.py

---
+++ Day 12/Part1.py
@@ -0,0 +1,27 @@
+if __name__ == "__main__":
+    with open("input.txt", "r") as l:
+        l = [[line[0], int(line.strip()[1:])] for line in l]
+    
+    dirs = {'N': [0,1], 'S': [0,-1], 'E': [1,0], 'W': [-1,0]}
+    direction = 0
+    coord = [0,0]
+    
+    for d, num in l:
+        if d in dirs:
+            coord[0] += dirs[d][0] * num
+            coord[1] += dirs[d][1] * num
+        elif d == "L":
+            direction = (direction + 360 - num) % 360
+        elif d == "R":
+            direction = (direction + num) % 360
+        else:
+            if direction == 0:
+                coord[0] += num
+            elif direction == 270:
+                coord[1] += num
+            elif direction == 180:
+                coord[0] -= num
+            elif direction == 90:
+                coord[1] -= num                
+    
+    print(abs(coord[0]) + abs(coord[1]))            
