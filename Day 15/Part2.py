14d868acd525e3dbc4ac865a207bb75fc10bfa7e
JoshWong1 <wongj09@gmail.com>
2021-01-29 01:09:43 -0500

    Create Part2.py

---
+++ Day 15/Part2.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+ 
+    l = open("input.txt", "r").read().strip().split(",")
+    d = {}
+    
+    for i,n in enumerate(l):
+        d[int(n)] = i  
+    last = l[i]
+    
+    while True:   
+        if i == 29999999:
+            print(last)
+            break        
+        if not last in d:
+            d[last] = i
+            last = 0
+        else:
+            temp = i - d[last]
+            d[last] = i
+            last = temp        
+        i += 1        
