4db7f762630dd22c9644ecadb90b2f3a74e29e69
JoshWong1 <wongj09@gmail.com>
2021-02-03 20:55:18 -0500

    Update Part1.py

--- Day 6/Part1.py
+++ Day 6/Part1.py
@@ -2,9 +2,9 @@ if __name__ == "__main__":
     sol = 0
     groups = open("input.txt", "r").read().split("\n\n")
     for group in groups:
-        found = []
+        found = set()
         for c in group:
             if c not in found and c != "\n":
-                found.append(c)
+                found.add(c)
                 sol += 1
     print(sol)
