f7fe9d5c0c94018096722ede600b972271fe0036
JoshWong1 <wongj09@gmail.com>
2021-02-03 20:35:04 -0500

    Update Part1.py

--- Day 2/Part1.py
+++ Day 2/Part1.py
@@ -1,24 +1,14 @@
-def isValid(low, high, letter, a):
+def isValid(low, high, letter, s):
     num = 0
-    for c in a:
-        if c == letter:
-            num += 1
-    if low <= num <= high:
-        return True
-    else:
-        return False
+    for c in s:
+        if c == letter: num += 1
+        
+    return low <= num <= high
     
 if __name__ == "__main__":
     count = 0
-    l = open("input.txt", "r").readlines()
-    for line in l:
-        items = line.split()
-        nums = items[0].split("-")
-        low = int(nums[0])
-        high = int(nums[1])
-        letter = items[1][0]
-        a = items[2]
-     
-        if isValid(low, high, letter, a):
-            count += 1
+    l = [line.split() for line in open("input.txt", "r").readlines()]
+    for r, c, s in l:       
+        low, high = r.split("-")     
+        count += isValid(int(low), int(high), c[0], s)
     print(count)
