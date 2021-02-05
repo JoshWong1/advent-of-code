36a43a75844aece7e0accb2dffd8a1941a40944c
JoshWong1 <wongj09@gmail.com>
2021-02-05 17:17:34 -0500

    Update Part2.py

--- Day 5/Part2.py
+++ Day 5/Part2.py
@@ -1,27 +1,17 @@
-
 def findRow(s, low, high):
     if len(s) <= 1:
-        if s == "F" or s == "L":
-            return low
-        else:
-            return high
-    mid = (high + low) // 2
+        return low if s in "FL" else high
     
-    if s[0] == "F" or s[0] == "L":        
-        return findRow(s[1:], low, mid)
-    else:       
-        return findRow(s[1:], mid + 1, high)
+    mid = (high + low) // 2
+    return findRow(s[1:], low, mid) if s[0] in "FL" else findRow(s[1:], mid + 1, high)
     
 if __name__ == "__main__":
     l = open("input.txt", "r").readlines()
     
-    ids = set()    
+    maxID = 0
     for seat in l:        
         row, col = findRow(seat[0:7], 0, 127), findRow(seat[7:10], 0, 7)
         ide = row * 8 + col
-        ids.add(ide)
-
-    for i in range(min(ids), max(ids)):
-        if i not in ids:
-            print(i)
-            break
+        maxID = max(maxID, ide)   
+        
+    print(maxID)
