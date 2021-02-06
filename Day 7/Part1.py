6f7122af37461449b96b6d51b33ee766f6268e8c
JoshWong1 <wongj09@gmail.com>
2021-02-06 15:03:50 -0500

    Update Part1.py

--- Day 7/Part1.py
+++ Day 7/Part1.py
@@ -4,27 +4,24 @@ def findAllColors(d, color, colors):
    total = 0
    for c in d[color]:
       if c not in colors:
-         colors.append(c)
+         colors.add(c)
          total += 1 + findAllColors(d, c, colors)
    return total        
 
 if __name__ == "__main__":
-   l = open("input.txt", "r").readlines()    
-   d = {}
-   colors = []
+   l = open("input.txt", "r").readlines()
    
+   d = {}     
    for line in l:
-      items = line.split("bags contain")
-      firstBag = items[0].strip()
-      sub = []
-      
-      for i in items[1].split(", "):
-         sub.append(i.strip()[1:].strip().split("bag")[0].strip())
-         for color in sub:
-            if color not in d:
-               d[color] = []
-            if firstBag not in d[color]:
-               d[color].append(firstBag)
-   
+      firstBag, bags = line.split(" bags contain ")  
+      for bag in bags.split(", "):
+         b = bag.split()
+         color = b[1] + " " + b[2]
+         if color not in d:
+            d[color] = {firstBag}
+         else:
+            d[color].add(firstBag)
+    
+   colors = set()
    n = findAllColors(d,'shiny gold', colors)
    print(n) 
