bd9bb644431c46ae8f2da6489b0d4397145c2afa
JoshWong1 <wongj09@gmail.com>
2021-02-19 17:07:52 -0500

    Update Part2(using regex).py

--- Day 18/Part2(using regex).py
+++ Day 18/Part2(using regex).py
@@ -1,9 +1,8 @@
 import re
 def solve(s):
     while "+" in s:
-        r = re.search('(\d)*\+(\d)*', s).group()
-        i = r.index("+")
-        s = s.replace(r, str(int(r[0:i]) + int(r[i+1:])))
+        r = re.search('(\d)*\+(\d)*', s).group()        
+        s = s.replace(r, str(eval(r)), 1)
     return eval(s)
 
 def solveP(s):
@@ -15,7 +14,6 @@ def solveP(s):
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         l = [line.strip().replace(" ", "") for line in f]
-
     sol = 0
     for s in l:
         x = solveP(s)        
