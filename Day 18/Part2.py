255fde589e4a3b5ef6f4cb0feae08868e4741b64
JoshWong1 <wongj09@gmail.com>
2021-02-19 22:21:16 -0500

    Update Part2.py

--- Day 18/Part2.py
+++ Day 18/Part2.py
@@ -1,25 +1,21 @@
 def applyOp(op, total, n):
-    if op:
-        return eval(str(total) + op + str(n))
-    else:
-        return n
-
+    return eval(str(total) + op + str(n)) if op else n
+  
 def solve(s, i):
     total = 0
     op = ""
     while i < len(s):        
         if s[i] in "+*":
             op = s[i]
-            i += 1
         elif s[i] == "(":
-            x = solve(s, i+1)
-            i = x[1] + 1
-            total = applyOp(op, total, x[0])
+            x, i = solve(s, i+1)
+            
+            total = applyOp(op, total, x)
         elif s[i] == ")":
             return total, i        
         else:
             total = applyOp(op, total, s[i])
-            i += 1     
+        i += 1     
     return total, i
 
 def findIndex(s, a, b, i, incr):
