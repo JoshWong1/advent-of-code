980e198c986275b1bc3e5fea20fb9896c1b8fea8
JoshWong1 <wongj09@gmail.com>
2021-02-19 22:00:22 -0500

    Update Part2(using regex).py

--- Day 18/Part2(using regex).py
+++ Day 18/Part2(using regex).py
@@ -1,7 +1,7 @@
 import re
 def solve(s):
     while "+" in s:
-        r = re.search('(\d)*\+(\d)*', s).group()        
+        r = re.search('(\d)* \+ (\d)*', s).group()        
         s = s.replace(r, str(eval(r)), 1)
     return eval(s)
 
@@ -13,7 +13,7 @@ def solveP(s):
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        l = [line.strip().replace(" ", "") for line in f]
+        l = [line.strip() for line in f]
     sol = 0
     for s in l:
         x = solveP(s)        
