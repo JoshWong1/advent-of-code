9933efaaa699ec957e87d0e0b01de5ea814345d1
JoshWong1 <wongj09@gmail.com>
2021-02-19 22:19:51 -0500

    Update Part1.py

--- Day 18/Part1.py
+++ Day 18/Part1.py
@@ -1,8 +1,5 @@
-def applyOp(op, total, n):
-    if op:
-        return eval(str(total) + op + str(n))
-    else:
-        return n
+def applyOp(op, total, n):    
+    return eval(str(total) + op + str(n)) if op else n
 
 def solve(s, i):
     total = 0
@@ -10,16 +7,14 @@ def solve(s, i):
     while i < len(s):        
         if s[i] in "+*":
             op = s[i]
-            i += 1
         elif s[i] == "(":
-            x = solve(s, i+1)
-            i = x[1] + 1
-            total = applyOp(op, total, x[0])
+            x, i= solve(s, i+1)
+            total = applyOp(op, total, x)
         elif s[i] == ")":
             return total, i        
         else:
             total = applyOp(op, total, s[i])
-            i += 1     
+        i += 1     
     return total, i
     
         
